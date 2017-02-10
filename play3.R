fa0a27b544547e250e2b4df1b9cb52c4f2a2451f
lidiabloshanskaya <lidia.bloshanskaya@gmail.com>
2017-02-09 19:36:35 -0500

    Add files via upload
    with mice imputation (creates overfit)

--- play3.R
+++ play3.R
@@ -3,6 +3,8 @@ suppressWarnings(suppressMessages(library('dplyr')))
 suppressWarnings(suppressMessages(library('randomForest')))
 suppressWarnings(suppressMessages(library('caret')))
 suppressWarnings(suppressMessages(library('ggthemes')))
+suppressWarnings(suppressMessages(library('mice')))  ## imputations
+suppressWarnings(suppressMessages(library('Amelia'))) ## stuff for missing values
 setwd("~/Dropbox/data science/kaggle - titanic")
 
 
@@ -21,13 +23,28 @@ test$Sex <-as.factor(test$Sex)
 train$Embarked <-as.factor(train$Embarked)
 test$Embarked <-as.factor(test$Embarked)
 
-##missing values for Age <- mean
+### MISSING VALUES: for Age <- mean
 
 train$Age[is.na(train$Age)] <- mean(train$Age,na.rm=T)
 test$Age[is.na(test$Age)] <- mean(test$Age,na.rm=T)
 test$Fare[is.na(test$Fare)]<- median(test$Fare,na.rm=T)
 
+### MISSING VALUES: age - PMM via mice
 
+print(sum(is.na(train$Age)))
+md.pattern(train)
+missmap(train)
+temp<-mice(train$Age,m=5,mazit=50,meth='pmm', seed=500)
+summary(temp)
+temp$imp$Age ##check the imputed data
+imputeTrain<-complete(temp)
+summary(imputeTrain)
+##visualize the original and imputed data
+par(mfrow=c(1,2)) # combine plots, also: layout(...)
+hist(train$Age)
+hist(imputeTrain$Age)
+
+### MISSING VALUES: Embarked
 missEmb.Id<-train$PassengerId[is.na(as.factor(train$Embarked))]
 missEmb.Fare<-train$Fare[is.na(as.factor(train$Embarked))] ##output 80 and 80
 missEmb.Pclass<-train$Pclass[is.na(as.factor(train$Embarked))] ## Pclass = 1 and 1 
@@ -69,7 +86,7 @@ ggplot(train, aes(train$Embarked, fill = factor(Survived))) +
 
 
 
-## choosing features: should figure out Embarked
+## choosing features
 features <- c("Pclass",
               "Age",
               "Sex",
@@ -78,17 +95,19 @@ features <- c("Pclass",
               "Fare",
               "Embarked")
 
-### break the train set on train + CV set: should use: createDataPartition 
-trainData <- train[1:800,]
-CV <- train[801:891,]
+### split the train set on train + CV set
+set.seed(1)
+splitIndex<-createDataPartition(train$PassengerId, p=.8, list = F, times=1)
+head(splitIndex)
+trainData<-train[splitIndex,]
+CV<-train[-splitIndex,]
 
-###linear regression
+###LOGISTIC REGRESSION########
 glm.model<-glm(Survived ~ Age+Pclass + Sex + SibSp +   
                  Parch + Fare+Embarked,
-               family=binomial(link='logit'),data=trainData)
+               family=binomial(link = "logit"),data=trainData)
 summary(glm.model)
 
-#predict on CV set
 glm.predict<-predict(glm.model, CV[,features],type='response')
 summary(glm.predict)
 glm.predict <- ifelse(glm.predict > 0.5,1,0)
@@ -105,3 +124,31 @@ glm.predict <- ifelse(glm.predict > 0.5,1,0)
 
 submission<-data.frame(PassengerID = test$PassengerId, Survived = glm.predict)
 write.csv(submission, file = 'glm_submission.csv', row.names = F)
+
+
+###### RANDOM FOREST #######
+ rf.model<-train(Survived ~ Age+Pclass + Sex + SibSp +   
+                   Parch + Fare + Embarked,
+                 data=trainData,
+                 method = "rf",
+                 trControl = trainControl(method = "cv", # Use cross-validation
+                                          number = 5) # Use 5 folds for cross-validation
+                 )
+rf.predict<-predict(rf.model,newdata=CV[,features])
+summary(rf.predict)
+rf.predict <- ifelse(rf.predict > 0.5,1,0)
+misClasificError <- mean(rf.predict != CV$Survived)
+print(paste('Accuracy',1-misClasificError)) ### gives 0.823
+##whole set
+rf.model<-train(Survived ~ Age+Pclass + Sex + SibSp +   
+                  Parch + Fare + Embarked,
+                data=train,
+                method = "rf",
+                trControl = trainControl(method = "cv", # Use cross-validation
+                                         number = 5) # Use 5 folds for cross-validation
+)
+rf.predict<-predict(rf.model, test[,features])
+rf.predict <- ifelse(rf.predict > 0.5,1,0)
+
+submission<-data.frame(PassengerID = test$PassengerId, Survived = rf.predict)
+write.csv(submission, file = 'rf_submission.csv', row.names = F)
